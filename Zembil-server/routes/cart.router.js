eabdecb8c8ab8c0e9149a85440dc56c9a7c8f855
yadu <yaduhat@gmail.com>
2020-06-12 23:58:39 -0500

    cart model and routes added

--- Zembil-server/routes/cart.router.js
+++ Zembil-server/routes/cart.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const cartConntroller = require('../controllers/cart.controller');
 
 
-router.post('/cart', async(req, res) = {
-    //  const { productId, quantity, name, price } = req.body;
 
+router.get('/', cartConntroller.getCart);
+router.post('/add-to-cart', cartConntroller.addToCart);
+router.delete('/delete-from-cart/:id', cartConntroller.deleteFromCart);
 
-})
+module.exports = router;
