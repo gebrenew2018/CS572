7982983ae9a059c7a3986b0ffc0f192ce6465be8
MihreteabKidane <45437899+MihreteabKidane@users.noreply.github.com>
2020-06-15 16:48:14 -0500

    getCreditCard details method updated

---
+++ zembil-client/src/app/models/creditCard.model.ts
@@ -0,0 +1,10 @@
+export class CreditCard{
+ // cardId: Number;
+  cardOwnerFirstName: String;
+  cardOwnerLastName: String;
+  billingAddress: String;
+  cardNumber: String;
+  expYear: string;
+  expMonth: Number;
+  csv: Number;
+}
