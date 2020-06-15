e709fbfdeaa7139fad969626b027fc8a49fa6c91
yadu <yaduhat@gmail.com>
2020-06-15 02:23:35 -0500

    updating the Shopping-Cart back-end

--- Zembil-server/controllers/cart.controller.js
+++ Zembil-server/controllers/cart.controller.js
@@ -4,92 +4,40 @@ const Product = mongoose.model('Product');
 
 
 exports.addToCart = async(req, res, next) => {
-    //req.user.id
-    console.log(req.body.products[0].productId);
-    const product = await Product.findById(req.body.products[0].productId);
-    console.log(product);
+    console.log(' right here in add to cart');
+    var productId = req.body.productId;
+    console.log(productId);
+    var cart = new Cart(req.body.cart ? req.body.cart : {});
 
-    const cart = new Cart({
-        _id: new mongoose.Types.ObjectId(),
-        userId: req.body.userId,
-        products: [{
-            productId: req.body.products[0].productId,
-            // price: req.body.products[0].price,
-            quantity: 1
-        }],
-        totalPrice: product.totalPrice
-    });
-    console.log(cart);
-    const currentCart = Cart.findById("5ee5622849b0a34aa09b5b71");
-    if (currentCart) {
-        console.log('cart already exist');
-        if (product) {
-
-            const isExisting = cart.products.findIndex(productIdInDB => {
-                return new String(productIdInDB.productId).trim() == new String(req.body.products[0].productId).trim()
-            });
-            if (isExisting >= 0) {
-                console.log(' greater than 0');
-                cart.products[isExisting].quantity += 1;
-                cart.totalPrice += product.unitPrice;
-            } else {
-                console.log(' lessthan than 0');
-                cart.products.push({ productId: product._id, quantity: 1 });
-            }
-            if (!cart.totalPrice) {
-                cart.totalPrice = 0;
-            }
-
-            cart.totalPrice += product.unitPrice;
-
-
-            //......
-            // return this.save()
-            cart.save((err, cart) => {
-                if (err) {
-                    res.status(500).json({ message: 'Error in adding to cart: ' + err });
-                } else if (!cart) {
-                    res.status(404).json({ message: 'Cart No Found' });
-                } else {
-                    res.status(200).json({ cart: cart });
-                }
-            })
-
-
-
+    Product.findById(productId, (err, product) => {
+        if (err) {
+            res.status(500).json({ message: 'Error in getting a product' });
         }
-    } else {
-        console.log("new cart")
-        cart.save((err, cart) => {
-            if (err) {
-                res.status(500).json({ message: 'Error in adding to cart: ' + err });
-            } else if (!cart) {
-                res.status(404).json({ message: 'Cart No Found' });
-            } else {
-                res.status(200).json({ cart: cart });
-            }
-        })
-    }
 
+        cart.add(product, productId);
+
+        // req.body.cart = cart.generateArray();
+        console.log(req.session.cart);
+        res.status(200).json({ MyCart: req.body.cart });
+
+    });
 
 }
 
 
 exports.getCart = (req, res, next) => {
-    Cart.find((err, cart) => {
-        if (err) {
-            res.status(500).json({ message: 'Error in get Cart ' });
-        } else if (!cart) {
-            res.status(404).json({ message: 'No Products Found' });
-
-        } else {
-            res.status(200).json({ cart: cart });
-        }
-
-    });
     console.log('get cart ');
 
+    if (!req.body.cart) {
+        res.status(200).json({ product: null });
+    }
+    var cart = new Cart(req.body.cart);
+    res.status(200).json({ products: cart.generateArray(), totalPrice: cart.totalPrice });
+
+
 }
+
+
 exports.deleteFromCart = async(req, res, next) => {
 
     console.log('delete cart ');
--- zembil-client/src/app/models/cart.model.ts
+++
@@ -1,6 +0,0 @@
-
-export class Cart{
-  cartId:number;
-products:String[];
-
- }
